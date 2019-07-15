cf1deceacbcf49fbb9ff024f489f5fc43d3fccbc
pmbaumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2019-07-14 22:43:09 -0400

    todo and requirements update

--- pipfreeze.sh
+++ pipfreeze.sh
@@ -1,2 +1,3 @@
 pip freeze > binder/requirements.txt
 grep -v "en-core-web" binder/requirements.txt > requirements; mv requirements binder/requirements.txt
+grep -v "certifi" binder/requirements.txt > requirements; mv requirements binder/requirements.txt
